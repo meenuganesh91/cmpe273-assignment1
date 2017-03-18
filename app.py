53ffc15f4af4ec62fec504acc86e8d4bb7cb94a4
Meenu Ganesh <meenuganesh91@gmail.com>
2017-03-17 23:26:24 -0700

    Pushing final changes

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import os
+
+import base64
 import sys
 from flask import Flask
-import yaml
 from github import Github
-import base64
-
-#docker run -t -i -p 5000:5000 assignment1-flask-app
+import yaml
+import json
 
 app = Flask(__name__)
 
@@ -13,30 +12,39 @@ app = Flask(__name__)
 def hello():
 	return "Hello from Dockerized Flask App!!"
 
-
-#Working!
 @app.route("/v1/<name>")
-def config(name):
-	app.config.update(
-    yaml.safe_load(open(os.path.join(os.getcwd(), name))))
-	return app.config["welcome_message"]
-
-@app.route("/<name>/")
 def gitConfig(name):
-	file_contents = repo.get_file_contents('/socket-mon.py')
-	file_data = file_contents.content
-	return base64.b64decode(file_data)
-
-
+	try:
+		if name.endswith(".yml"):
+			file_contents = repo.get_file_contents('/' + name)
+			file_data = file_contents.content
+			return base64.b64decode(file_data)
+		elif name.endswith(".json"):
+			name = name.replace(".json", ".yml")
+			file_contents = repo.get_file_contents('/' + name)
+			file_data = json.dumps(yaml.load(base64.b64decode(file_contents.content)), sort_keys=False, indent=2)
+			return file_data
+		else:
+			return "Incorrect file format!!\nFile should be in yml or json format.\n"
+	
+	except:
+		return "File not found in repository!!\n"
+	
+	
 if __name__ == "__main__":
-	"""
-	print 'Number of arguments:', len(sys.argv), 'arguments.'
-	print 'Argument List:', str(sys.argv)
-	"""
-	client = Github('090a38d9a6c345c2aa70a853be86b926926bcfc5')
-	user = client.get_user('meenuganesh91')
-	repo = user.get_repo('CMPE-273-Lab1')
-
-	file_contents = repo.get_file_contents('/socket-mon.py')
-	file_data = file_contents.content
-	app.run(debug=True, host='0.0.0.0')
+	if len(sys.argv) < 2:
+		print "Github url needed to process requests"
+		exit()
+	else:
+		repo_url = str(sys.argv[1])
+		repo_url = repo_url.split("/")
+		try:
+			username = repo_url[repo_url.index("github.com")+1]
+			repo_name = repo_url[repo_url.index(username)+1]
+			client = Github("meenuganesh91", "AmenRedWim3")
+			user = client.get_user(username)
+			repo = user.get_repo(repo_name)
+			app.run(debug=True, host='0.0.0.0')
+		except:
+			print "Github url needed to process requests"
+			exit()
